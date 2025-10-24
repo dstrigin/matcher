5cd22b3c7d6d036d13974ecb5dc96ffabc5f88cd
TRCMKR <alexplay1224@gmail.com>
2025-10-24 22:19:00 +0300

    [matcher-back] add handler template

--- matcher-back/internal/web/handlers/users/post_users.go
+++ matcher-back/internal/web/handlers/users/post_users.go
@@ -1,1 +1,14 @@
 package users
+
+import (
+	"context"
+	"net/http"
+)
+
+type postUsersRequest struct {
+}
+
+func PostUsersHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
